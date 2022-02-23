fd6fd601dc94e183cdf1d9f9195629b5a31d5a7f
rodrigojgq <rodrigojgq@gmail.com>
2022-02-22 23:23:18 -0400

    🔧[ADD] ESLint and Prettier Config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React from 'react';
 import '../styles/components/app.css';
 
 const App = () => {
-  return (
-    <h1>Hola Mundo</h1>
-  )
-}
+  return <h1>Hola Mundo</h1>;
+};
 
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   mode: 'development',
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
@@ -18,16 +18,16 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader'
-          }
-        ]
+            loader: 'html-loader',
+          },
+        ],
       },
       {
         test: /\.css$/,
@@ -35,23 +35,23 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
           },
-          'css-loader'
-        ]
-      }
-    ]
+          'css-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].css'
-    })
+      filename: 'assets/[name].css',
+    }),
   ],
   devServer: {
     static: path.join(__dirname, 'dist'),
     compress: true,
     port: 3005,
-  }
-}
+  },
+};
